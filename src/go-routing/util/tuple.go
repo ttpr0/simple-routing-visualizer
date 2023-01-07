b21a733de515a9ea09497601c096cbfbe739c212
ttpr0 <ttpro@gmx.de>
2023-01-07 09:32:48 +0100

    added Tuple and Triple

---
+++ src/go-routing/util/tuple.go
@@ -0,0 +1,20 @@
+package util
+
+type Tuple[V1, V2 any] struct {
+	A V1
+	B V2
+}
+
+type Triple[V1, V2, V3 any] struct {
+	A V1
+	B V2
+	C V3
+}
+
+func MakeTuple[V1, V2 any](itemA V1, itemB V2) Tuple[V1, V2] {
+	return Tuple[V1, V2]{itemA, itemB}
+}
+
+func MakeTriple[V1, V2, V3 any](itemA V1, itemB V2, itemC V3) Triple[V1, V2, V3] {
+	return Triple[V1, V2, V3]{itemA, itemB, itemC}
+}
