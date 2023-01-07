4cda05313305411d543ef47ade28107050362e68
ttpr0 <ttpro@gmx.de>
2023-01-07 09:23:49 +0100

    added Clear method

--- src/go-routing/util/priority_queue.go
+++ src/go-routing/util/priority_queue.go
@@ -12,20 +12,16 @@ type _PriorityQueueItem[T any, P number] struct {
 type PriorityQueue[T any, P number] []_PriorityQueueItem[T, P]
 
 func (self *PriorityQueue[T, P]) Len() int { return len(*self) }
-
 func (self *PriorityQueue[T, P]) Less(i, j int) bool {
 	return (*self)[i].priority < (*self)[j].priority
 }
-
 func (self *PriorityQueue[T, P]) Swap(i, j int) {
 	(*self)[i], (*self)[j] = (*self)[j], (*self)[i]
 }
-
 func (self *PriorityQueue[T, P]) Push(x any) {
 	item := x.(_PriorityQueueItem[T, P])
 	*self = append(*self, item)
 }
-
 func (self *PriorityQueue[T, P]) Pop() any {
 	old := *self
 	n := len(old)
@@ -33,17 +29,14 @@ func (self *PriorityQueue[T, P]) Pop() any {
 	*self = old[0 : n-1]
 	return item
 }
-
 func (self *PriorityQueue[T, P]) Update(item int, value T, priority P) {
 	(*self)[item].value = value
 	(*self)[item].priority = priority
 	heap.Fix(self, item)
 }
-
 func (self *PriorityQueue[T, P]) Enqueue(value T, priority P) {
 	heap.Push(self, _PriorityQueueItem[T, P]{value: value, priority: priority})
 }
-
 func (self *PriorityQueue[T, P]) Dequeue() (T, bool) {
 	if len(*self) == 0 {
 		var result T
@@ -51,6 +44,9 @@ func (self *PriorityQueue[T, P]) Dequeue() (T, bool) {
 	}
 	item := heap.Pop(self).(_PriorityQueueItem[T, P])
 	return item.value, true
+}
+func (self *PriorityQueue[T, P]) Clear() {
+	*self = (*self)[:0]
 }
 
 func NewPriorityQueue[T any, P number](length int) PriorityQueue[T, P] {
