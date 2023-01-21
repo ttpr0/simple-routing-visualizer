2bef6eef654f026bd42ea360a6bc5dd0674f695c
ttpr0 <ttpro@gmx.de>
2023-01-21 18:32:05 +0100

    updated AddToMap contextmenu

--- src/web-app/components/contextmenu/AddToMap.ts
+++ src/web-app/components/contextmenu/AddToMap.ts
@@ -1,7 +1,11 @@
 import { ref, reactive, computed, watch, onMounted } from 'vue';
 import { getAppState, getMapState } from '/state';
 import { getMap } from '/map';
+import { VectorLayer } from '/map/VectorLayer';
+import { PointStyle, LineStyle, PolygonStyle } from '/map/style';
 import { topbarbutton } from '/share_components/topbar/TopBarButton';
+import { getConnectionManager } from '/components/sidebar/filebar/ConnectionManager';
+import { getKeyFromPath, getPathFromPath } from '/util';
 
 const add_to_map = {
   components: { topbarbutton },
@@ -11,8 +15,28 @@ const add_to_map = {
     const state = getAppState();
     const map = getMap();
     const map_state = getMapState();
+    const connmanager = getConnectionManager();
 
-    function addToMap() {
+    async function addToMap() {
+      let key = getKeyFromPath(state.contextmenu.context.path);
+      let path = getPathFromPath(state.contextmenu.context.path) + state.contextmenu.context.name;
+      const conn = connmanager.getConnection(key);
+      const geojson = await conn.openFile(path);
+      const features = geojson["features"];
+      console.log(features);
+      const type = features[0].geometry.type;
+      const layername = prompt("Please enter a Layer-Name", "");
+      let layer = null;
+      if (["Point", "MultiPoint"].includes(type)) {
+        layer = new VectorLayer(features, "Point", layername, new PointStyle());
+      }
+      else if (["LineString", "MultiLineString"].includes(type)) {
+        layer = new VectorLayer(features, "LineString", layername, new LineStyle());
+      }
+      else if (["Polygon", "MultiPolygon"].includes(type)) {
+        layer = new VectorLayer(features, "Polygon", layername, new PolygonStyle());
+      }
+      map.addLayer(layer);
       state.contextmenu.display = false;
     }
 
