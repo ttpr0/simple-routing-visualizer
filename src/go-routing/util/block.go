38e58fb3e62918580e2f32de37535f5218920eed
ttpr0 <ttpro@gmx.de>
2023-03-18 18:18:23 +0100

    added locks for safety

--- src/go-routing/util/block.go
+++ src/go-routing/util/block.go
@@ -1,19 +1,27 @@
 package util
 
+import "sync"
+
 type Block struct {
-	channel   chan bool
-	is_locked bool
+	channel      chan bool
+	is_locked    bool
+	take_lock    sync.Mutex
+	release_lock sync.Mutex
 }
 
 func (self *Block) Take() {
+	self.take_lock.Lock()
+	defer self.take_lock.Unlock()
 	self.Release()
 	self.is_locked = true
 	<-self.channel
-	self.is_locked = false
 }
 func (self *Block) Release() {
+	self.release_lock.Lock()
+	defer self.release_lock.Unlock()
 	if self.is_locked {
 		self.channel <- true
+		self.is_locked = false
 	}
 }
 func (self *Block) IsTaken() bool {
