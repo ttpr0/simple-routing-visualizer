a4a0e6a0e368fb0637362c892f16b37d167d16d6
ttpr0 <ttpro@gmx.de>
2023-07-11 14:15:03 +0200

    added share components

---
+++ src/web-app/share_components/TextInput.vue
@@ -0,0 +1,53 @@
+<script lang="ts">
+import { createApp, ref, reactive, onMounted, computed, watch } from "vue";
+
+export default {
+  components: {
+  },
+  props: {
+    "placeholder": {
+        type: String,
+        default: ""
+    },
+    "padding": {
+        type: String,
+        default: "10px"
+    },
+    "modelValue": {
+        type: String,
+        default: ""
+    }
+  },
+  emits: [
+    "update:modelValue"
+  ],
+  setup(props, ctx) {
+
+    function oninput(e) {
+        ctx.emit("update:modelValue", e.target.value);
+    }
+
+    return { oninput };
+  }
+};
+</script>
+
+<template>
+    <input type="text" class="input-field" :value="modelValue" @input="oninput" :placeholder="placeholder" :style="{padding: padding}">
+</template>
+
+<style scoped>
+.input-field {
+  border: none;
+  border-radius: 4px;
+  background-color: var(--bg-color);
+  color: var(--text-color);
+  width: 100%;
+}
+
+.input-field:focus {
+  outline: none;
+  background-color: var(--bg-light-color);
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ src/web-app/share_components/accordion/Accordion.vue
@@ -0,0 +1,32 @@
+<script>
+
+function sleep (ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export default {
+    components: {
+    },
+    props: {
+    },
+    emits: [
+    ],
+    setup() {
+
+        return {};
+    }
+};
+</script>
+
+<template>
+    <div class="accordion">
+        <slot />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.accordion {
+  font-family: Arial, sans-serif;
+  color: #333;
+}
+</style>
