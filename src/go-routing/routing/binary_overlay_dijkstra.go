6b1bb649eb67229fe7422fdc4ba406def23ea49b
ttpr0 <ttpro@gmx.de>
2023-08-28 19:03:48 +0200

    updated binary-overlay dijkstra

--- src/go-routing/routing/binary_overlay_dijkstra.go
+++ src/go-routing/routing/binary_overlay_dijkstra.go
@@ -12,6 +12,7 @@ type _FlagBOD struct {
 	curr_node   int32
 	path_length float64
 	prev_edge   int32
+	is_shortcut bool
 	visited     bool
 	skip        bool
 }
@@ -66,18 +67,11 @@ func (self *BODijkstra) CalcShortestPath() bool {
 		} else {
 			edges = explorer.GetAdjacentEdges(curr_id, graph.FORWARD, graph.ADJACENT_ALL)
 		}
-		// edges := explorer.GetAdjacentEdges(curr_id, graph.FORWARD, graph.ADJACENT_ALL)
 		for {
 			ref, ok := edges.Next()
 			if !ok {
 				break
 			}
-			if !ref.IsEdge() {
-				continue
-			}
-			// if curr_flag.skip && !ref.IsCrossBorder() && !ref.IsSkip() {
-			// 	continue
-			// }
 			edge_id := ref.EdgeID
 			other_id := ref.OtherID
 			var other_flag _FlagBOD
@@ -89,7 +83,7 @@ func (self *BODijkstra) CalcShortestPath() bool {
 			if other_flag.visited {
 				continue
 			}
-			new_length := curr_flag.path_length + float64(self.weight.GetEdgeWeight(edge_id))
+			new_length := curr_flag.path_length + float64(explorer.GetEdgeWeight(ref))
 			if other_flag.path_length > new_length {
 				if ref.IsCrossBorder() {
 					tile_id := self.graph.GetNodeTile(other_id)
@@ -103,6 +97,7 @@ func (self *BODijkstra) CalcShortestPath() bool {
 				}
 				other_flag.curr_node = other_id
 				other_flag.prev_edge = edge_id
+				other_flag.is_shortcut = ref.IsShortcut()
 				other_flag.path_length = new_length
 				self.heap.Enqueue(other_flag, new_length)
 			}
@@ -123,7 +118,6 @@ func (self *BODijkstra) Steps(count int, visitededges *List[geo.CoordArray]) boo
 		if curr_id == self.end_id {
 			return false
 		}
-		//curr := (*d.graph).GetNode(curr_id)
 		if self.flags.ContainsKey(curr_id) {
 			temp_flag := self.flags.Get(curr_id)
 			if temp_flag.path_length < curr_flag.path_length {
@@ -132,17 +126,16 @@ func (self *BODijkstra) Steps(count int, visitededges *List[geo.CoordArray]) boo
 		}
 		curr_flag.visited = true
 		self.flags.Set(curr_id, curr_flag)
-		edges := explorer.GetAdjacentEdges(curr_id, graph.FORWARD, graph.ADJACENT_ALL)
+		var edges IIterator[graph.EdgeRef]
+		if curr_flag.skip {
+			edges = explorer.GetAdjacentEdges(curr_id, graph.FORWARD, graph.ADJACENT_SKIP)
+		} else {
+			edges = explorer.GetAdjacentEdges(curr_id, graph.FORWARD, graph.ADJACENT_ALL)
+		}
 		for {
 			ref, ok := edges.Next()
 			if !ok {
 				break
-			}
-			if !ref.IsEdge() {
-				continue
-			}
-			if curr_flag.skip && !ref.IsCrossBorder() && !ref.IsSkip() {
-				continue
 			}
 			edge_id := ref.EdgeID
 			other_id := ref.OtherID
@@ -155,7 +148,10 @@ func (self *BODijkstra) Steps(count int, visitededges *List[geo.CoordArray]) boo
 			if other_flag.visited {
 				continue
 			}
-			visitededges.Add(self.geom.GetEdge(edge_id))
+			if ref.IsShortcut() {
+			} else {
+				visitededges.Add(self.geom.GetEdge(edge_id))
+			}
 			new_length := curr_flag.path_length + float64(self.weight.GetEdgeWeight(edge_id))
 			if other_flag.path_length > new_length {
 				if ref.IsCrossBorder() {
@@ -170,6 +166,7 @@ func (self *BODijkstra) Steps(count int, visitededges *List[geo.CoordArray]) boo
 				}
 				other_flag.curr_node = other_id
 				other_flag.prev_edge = edge_id
+				other_flag.is_shortcut = ref.IsShortcut()
 				other_flag.path_length = new_length
 				self.heap.Enqueue(other_flag, new_length)
 			}
@@ -182,7 +179,7 @@ func (self *BODijkstra) Steps(count int, visitededges *List[geo.CoordArray]) boo
 func (self *BODijkstra) GetShortestPath() Path {
 	explorer := self.graph.GetDefaultExplorer()
 
-	path := make([]int32, 0, 10)
+	path := NewList[int32](10)
 	length := int32(self.flags[self.end_id].path_length)
 	curr_id := self.end_id
 	var edge int32
@@ -190,9 +187,15 @@ func (self *BODijkstra) GetShortestPath() Path {
 		if curr_id == self.start_id {
 			break
 		}
-		edge = self.flags[curr_id].prev_edge
-		path = append(path, edge)
-		curr_id = explorer.GetOtherNode(graph.CreateEdgeRef(edge), curr_id)
+		curr_flag := self.flags[curr_id]
+		edge = curr_flag.prev_edge
+		if curr_flag.is_shortcut {
+			// self.graph.GetEdgesFromShortcut(&path, edge)
+			curr_id = explorer.GetOtherNode(graph.CreateCHShortcutRef(edge), curr_id)
+		} else {
+			path.Add(edge)
+			curr_id = explorer.GetOtherNode(graph.CreateEdgeRef(edge), curr_id)
+		}
 	}
 	for i, j := 0, len(path)-1; i < j; i, j = i+1, j-1 {
 		path[i], path[j] = path[j], path[i]
