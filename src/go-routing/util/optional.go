a8a2c97718830462b4aacde4d0ac1a499862cd1d
ttpr0 <ttpro@gmx.de>
2023-10-08 12:51:04 +0200

    added optional type

---
+++ src/go-routing/util/optional.go
@@ -0,0 +1,28 @@
+package util
+
+type Optional[T any] struct {
+	Value      T
+	_has_value bool
+}
+
+// Returns if the optional value is set.
+func (self *Optional[T]) HasValue() bool {
+	return self._has_value
+}
+
+// Creates an optional with set value.
+func Ok[T any](value T) Optional[T] {
+	return Optional[T]{
+		Value:      value,
+		_has_value: true,
+	}
+}
+
+// Creates an unset optional.
+func None[T any]() Optional[T] {
+	var value T
+	return Optional[T]{
+		Value:      value,
+		_has_value: false,
+	}
+}
